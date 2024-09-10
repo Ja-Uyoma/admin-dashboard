d84b2c20dccc74e45f23a8cd20e601668c150b1f
Jimmy Givans <jauyoma.dev@gmail.com>
2024-09-10 16:57:55 +0300

    feat: create custom list image types using icons

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 export default {
   content: ["./index.html", "./src/main.ts"],
   theme: {
-    extend: {},
+    extend: {
+      listStyleImage: {
+        home: "url('./src/images/home.svg')",
+        profile: "url('./src/images/face-man-profile.svg')",
+        messages: "url('./src/images/message-reply.svg')",
+        history: "url('./src/images/clock-time-three-outline.svg')",
+        tasks: "url('./src/images/note-multiple.svg')",
+        communities: "url('./src/images/account-group.svg')",
+        settings: "url('./src/images/cog.svg')",
+        support: "url('./src/images/help-box.svg')",
+        privacy: "url('./src/images/shield-check.svg')",
+      },
+    },
   },
   plugins: [],
 };
